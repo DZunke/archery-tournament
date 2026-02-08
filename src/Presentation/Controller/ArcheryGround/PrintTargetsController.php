db5ef55ff9e8ad75437689aee07497d3221b3ce6
Denis Zunke <denis.zunke@gmail.com>
2026-02-08 19:19:45 +0100

    feat(print): add print targets functionality and update show page with print links

---
+++ src/Presentation/Controller/ArcheryGround/PrintTargetsController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presentation\Controller\ArcheryGround;
+
+use App\Application\Bus\QueryBus;
+use App\Application\Query\ArcheryGround\GetArcheryGround;
+use App\Domain\Entity\ArcheryGround;
+use App\Domain\ValueObject\TargetType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PrintTargetsController extends AbstractController
+{
+    public function __construct(private readonly QueryBus $queryBus)
+    {
+    }
+
+    #[Route('/archery-grounds/{id}/print-targets', name: 'archery_ground_print_targets', methods: ['GET'])]
+    public function __invoke(string $id): Response
+    {
+        $archeryGround = $this->queryBus->ask(new GetArcheryGround($id));
+        if (! $archeryGround instanceof ArcheryGround) {
+            throw $this->createNotFoundException('Archery ground not found.');
+        }
+
+        return $this->render('archery_ground/print_targets.html.twig', [
+            'archeryGround' => $archeryGround,
+            'targetTypes' => TargetType::cases(),
+        ]);
+    }
+}
